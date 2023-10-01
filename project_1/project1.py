1f289167a5843d6a0f6197edd7e29f86f7d8863b
SDorny <sarahdorny@gmail.com>
2023-09-30 23:05:34 -0600

    final project 1

--- project_1/project1.py
+++ project_1/project1.py
@@ -11,9 +11,18 @@ names = pd.read_csv(url)
 
 
 # %%
+##########################################
+# BASIC TABLE OF THE DATA
+##########################################
 names
 
+
  # %%
+##########################################
+# QUESTION 1 - QUERY
+##########################################
+
+
 sarah_over_time = (names
                  .filter(['name', 'year', 'Total'])
                  .query('name == "Sarah"'))
@@ -22,8 +31,14 @@ count = sarah_over_time['Total'].sum()
 sarah_over_time
 
 
-
 # %%
+##########################################
+# QUESTION 1 - CHART
+##########################################
+# Data for reference line and annotation
+ref_data = pd.DataFrame({'year': [1999], 'Total': [sarah_over_time['Total'].max()], 'text': ['My Birth Year']})
+
+# Main chart
 chart = alt.Chart(sarah_over_time).mark_area(
     line={'color':'darkgreen'},
     color=alt.Gradient(
@@ -36,14 +51,28 @@ chart = alt.Chart(sarah_over_time).mark_area(
         y2=0
     )
 ).encode(
-    alt.X('year', axis= alt.Axis(format = 'd')),
-    alt.Y('Total')
-).properties(title= "Name of Sarah Overtime")
+    x=alt.X('year', axis= alt.Axis(format = 'd'), title='Year'),
+    y=alt.Y('Total', title='# of Births')
+).properties(title= "The Name 'Sarah' Spikes in Popularity")
 
+# Reference line and text annotation
+annotations = alt.Chart(ref_data).mark_rule(color='red').encode(x='year:Q') + \
+              alt.Chart(ref_data).mark_text(align='left', baseline='middle', dx=2).encode(
+                  x='year:Q',
+                  y='Total:Q',
+                  text='text'
+              )
 
-chart
+# Combine chart and annotations
+sarah_chart = chart + annotations
+
+sarah_chart
+
 
 # %%
+##########################################
+# QUESTION 2 - QUERY
+##########################################
 brit_over_time = (names
                  .filter(['name', 'year', 'Total'])
                  .query('name == "Brittany"'))
@@ -52,54 +81,105 @@ brit_over_time
 
 
 # %%
+##########################################
+# QUESTION 2 - CHART
+##########################################
 chart = alt.Chart(brit_over_time).mark_area(
-    line={'color':'darkgreen'},
+    line={'color':'blue'},
     color=alt.Gradient(
         gradient='linear',
         stops=[alt.GradientStop(color='white', offset=0),
-               alt.GradientStop(color='darkgreen', offset=1)],
+               alt.GradientStop(color='blue', offset=1)],
         x1=1,
         x2=1,
         y1=1,
         y2=0
     )
 ).encode(
-    alt.X('year', axis= alt.Axis(format = 'd')),
-    alt.Y('Total')
-)
+    x=alt.X('year', axis= alt.Axis(format = 'd'), title='Year'),
+    y=alt.Y('Total', title='# of Births')
+).properties(title= "'Brittany', a Name Belonging to the 80's and 90's Kids")
 
-chart
+# Data for reference lines (without text annotations)
+ref_data = pd.DataFrame({'year': [1980, 2000]})
 
-# LIMIT THE X-AXIS TO THE SPECIFIED YEARS
+# Reference lines
+reference_lines = alt.Chart(ref_data).mark_rule(color='orange').encode(x='year:Q')
+
+# Combine chart and reference lines
+brit_chart = chart + reference_lines
+
+brit_chart
+
+
+
 # %%
+##########################################
+# QUESTION 3 - QUERY
+##########################################
 filterd_names = (names
                  .filter(['name', 'year', 'Total'])
                  .query('name == "Paul" or name == "Peter" or name == "Martha" or name == "Mary"')
+                 .query('1920 <= year <= 2000')  # Add only the dates we need
                  )
 
-filterd_names
 
 # %%
+##########################################
+# QUESTION 3 - CHART
+##########################################
 chart = alt.Chart(filterd_names).mark_line().encode(
-    x=alt.X('year', axis= alt.Axis(format = 'd')),
-    y='Total',
+    x=alt.X('year', axis= alt.Axis(format = 'd'), title='Year'),
+    y=alt.Y('Total', title='# of Births'),
     color=alt.Color('name').title("Names"),
-)
+).properties(title= "Huge Decline in Top 4 Gospel Names in the 60's")
 chart
 
 
+
 # %%
+##########################################
+# QUESTION 4 - QUERY
+##########################################
 leia_over_time = (names
                  .filter(['name', 'year', 'Total'])
                  .query('name == "Leia"'))
-count = leia_over_time['Total'].sum()
 
-leia_over_time
-# %%
+##########################################
+# QUESTION 4 - CHART
+##########################################
 chart = alt.Chart(leia_over_time).mark_line().encode(
     x=alt.X('year', axis= alt.Axis(format = 'd')),
     y='Total',
     color=alt.Color('name').title("Name"),
 )
-chart
+
+# Data for annotations and reference lines
+annotations_data = pd.DataFrame({
+    'year': [1977, 1999, 2015],
+    'Total': [120, 120, 50],  # My Annotations location on the y-axis
+    'text': ["'A New Hope' comes out", "Prequels come out", "'The Force Awakens' comes out"]
+})
+
+# Annotations
+annotations = alt.Chart(annotations_data).mark_text(
+    align='left',
+    baseline='middle',
+    dx=1,  # Nudges text to right so it doesn't appear on top of the bar
+    dy=-5,  # Nudges text upward so it appears above the bar
+    angle=270  # Rotates text 270 degrees
+).encode(
+    x='year:Q',
+    y='Total:Q',  # Use the y values specified in annotations_data
+    text='text'
+).properties(title= "With Each Star Wars Movie, Leia Becomes a More Popular Name")
+
+# Reference lines
+reference_lines = alt.Chart(annotations_data).mark_rule(color='orange').encode(x='year:Q')
+
+# Combine chart, annotations, and reference lines
+leia_chart = chart + annotations + reference_lines
+
+leia_chart
+
 # %%
