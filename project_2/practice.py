178e7c612cd36002078542bcaad2aac998f08af6
SDorny <sarahdorny@gmail.com>
2023-10-03 22:41:08 -0600

    p2 update class

--- project_2/practice.py
+++ project_2/practice.py
@@ -7,13 +7,20 @@ from IPython.display import Markdown
 from IPython.display import display
 from tabulate import tabulate
 
-data = pd.read_csv("https://raw.githubusercontent.com/byuidatascience/data4missing/master/data-raw/flights_missing/flights_missing.json")
-
-data
+data = pd.read_json("https://raw.githubusercontent.com/byuidatascience/data4missing/master/data-raw/flights_missing/flights_missing.json")
 
 # %%
+#data.head()
+#data.info()
 
-data.dtypes
+
 # %%
-data.info()
+data.describe()
+
+# %%
+data.value_counts()
+
+#%%
+data.isna().sum()
+
 # %%
