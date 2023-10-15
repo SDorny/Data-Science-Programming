30c9eb16ea5d2a717bdb9c77d4ea5a01d8661560
SDorny <sarahdorny@gmail.com>
2023-10-14 19:11:16 -0600

    question 1 complete P2

--- project_2/practice.py
+++ project_2/practice.py
@@ -1,4 +1,7 @@
 #%%
+##########################################
+# Import Libraries and JSON
+##########################################
 import pandas as pd
 import altair as alt
 import numpy as np
@@ -9,18 +12,101 @@ from tabulate import tabulate
 
 data = pd.read_json("https://raw.githubusercontent.com/byuidatascience/data4missing/master/data-raw/flights_missing/flights_missing.json")
 
-# %%
-#data.head()
-#data.info()
-
 
 # %%
-data.describe()
+##########################################
+# Look at the data and get our bearings
+##########################################
+data.head()
 
 # %%
-data.month.unique()
+##########################################
+# QUESTION 1
+# Which airport has the worst delays?
+# SAN FRANCISCO
+##########################################
+
+worst_airport = data.groupby('airport_name')[['num_of_flights_total', 'num_of_delays_total', 'minutes_delayed_total']].sum()
+
+worst_airport
 
 #%%
-data.isna().sum().sum()
+
+# Creating our new column
+worst_airport['proportion_delays'] = worst_airport['num_of_delays_total']/worst_airport['num_of_flights_total']
+
+# Average delay time 
+worst_airport['avg_delayed_time_hrs'] = worst_airport['minutes_delayed_total']/60
+
+worst_airport
+
+#%%
+# Assuming worst_airport is your DataFrame
+worst_airport = worst_airport.sort_values('proportion_delays', ascending=False)
+
+# Convert 'proportion_delays' to a percentage
+worst_airport['proportion_delays'] = worst_airport['proportion_delays'].apply(lambda x: '{:.2%}'.format(x))
+
+# Convert 'avg_delayed_time_hrs' to a whole number
+worst_airport['avg_delayed_time_hrs'] = worst_airport['avg_delayed_time_hrs'].astype(int)
+
+worst_airport
+
+#%%
+##########################################
+# QUESTION 2
+# Best month to fly?
+# SAN FRANCISCO
+##########################################
+
+
+
+
+
+
+
+
+
+
+
+
 
 # %%
+##########################################
+# Replace 'n/a' values with NaN
+##########################################
+data = data.replace('n/a', np.nan)
+
+# Check locations of NaN values
+data.isna().sum()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+####################################################
+# Remove any rows that have missing Month data
+####################################################
+monthdata = data[data['month'].notna()]
+
+# Make sure that worked
+monthdata.isna().sum()
+
+
+# %%
+##########################################
+# 
+##########################################
