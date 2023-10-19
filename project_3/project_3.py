df58fec2579f9a5e98d19c9539f777cdfee03382
SDorny <sarahdorny@gmail.com>
2023-10-18 20:11:42 -0600

    Method Checkpoint

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -7,4 +7,12 @@ con = sqlite3.connect('lahmansbaseballdb.sqlite')
 df = pd.read_sql_query('SELECT * FROM fielding LIMIT 5;', con)
 
 df
+
+
+# %%
+con = sqlite3.connect('lahmansbaseballdb.sqlite')
+df = pd.read_sql_query('SELECT AB, H FROM batting LIMIT 2;', con)
+
+df
+
 # %%
