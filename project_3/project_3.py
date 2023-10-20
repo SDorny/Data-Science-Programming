997e31aa8bfd84ca93406cab76147c3c04f5955d
SDorny <sarahdorny@gmail.com>
2023-10-20 14:23:28 -0600

    beginning assignment

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -16,3 +16,23 @@ df = pd.read_sql_query('SELECT AB, H FROM batting LIMIT 2;', con)
 df
 
 # %%
+df = pd.read_sql_query('SELECT * FROM Schools WHERE city == "Rexburg";', con)
+
+df
+# %%
+#########################
+# QUESTION 1
+# schoolID = idbyuid
+# name_full = Brigham Young University-Idaho
+#########################
+df = pd.read_sql_query(
+    'SELECT playerID, schoolID, yearID FROM CollegePlaying WHERE schoolID == "idbyuid";', con)
+
+df
+
+# %%
+df = pd.read_sql_query(
+    'SELECT playerID, schoolID, yearID, salary, teamID FROM CollegePlaying LEFTJOIN Salaries ON playerID WHERE schoolID == "idbyuid";', con)
+
+df
+# %%
