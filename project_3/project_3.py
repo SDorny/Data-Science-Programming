394f476ccff9b89288979fa18429f4b1dd921bad
SDorny <sarahdorny@gmail.com>
2023-10-20 18:33:35 -0600

    halfway point

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -1,6 +1,8 @@
 #%%
 import pandas as pd
 import sqlite3
+import altair as alt
+alt.data_transformers.enable("vegafusion")
 
 con = sqlite3.connect('lahmansbaseballdb.sqlite')
 
@@ -15,7 +17,7 @@ query1 = pd.read_sql_query(
     FROM CollegePlaying
     JOIN Salaries
     ON Salaries.playerID = CollegePlaying.playerID
-    WHERE CollegePlaying.schoolID == 'idbyuid'
+    WHERE CollegePlaying.schoolID = 'idbyuid'
     ORDER BY Salaries.salary DESC;''', 
     con)
 
@@ -40,7 +42,7 @@ college
 ######################################################################################################################################################
 #%%
 #########################
-# QUESTION 2
+# QUESTION 2A
 # Probably need to clean data
 #########################
 query2 = pd.read_sql_query(
@@ -61,7 +63,75 @@ query2
 # CollegePlaying Table
 ###########################
 batting = pd.read_sql_query(
-    'SELECT * FROM Batting;', con)
+    '''SELECT AB
+    FROM Batting
+    WHERE H > 0;''', con)
 
 batting
+# %%
+#########################
+# QUESTION 2B
+# Probably need to clean data
+#########################
+query2 = pd.read_sql_query(
+    '''SELECT Salaries.playerID, CAST((Batting.H/Batting.AB) AS FLOAT) AS battingAvg, Salaries.yearID
+    FROM Batting
+    JOIN Salaries
+    ON Salaries.playerID = Batting.playerID
+    WHERE Batting.AB > 10 AND Salaries.yearID = 2001
+    ORDER BY battingAvg DESC
+    LIMIT 5;''', 
+    con)
+
+query2
+# %%
+#########################
+# QUESTION 2C
+# Probably need to clean data
+#########################
+query2 = pd.read_sql_query(
+    '''SELECT 
+    FROM Batting
+    JOIN Salaries
+    ON Salaries.playerID = Batting.playerID
+    WHERE Batting.AB > 10 AND Salaries.yearID = 2001
+    ORDER BY battingAvg DESC
+    LIMIT 5;''', 
+    con)
+
+query2
+
+#%%
+###########################
+# Teams Table
+###########################
+team = pd.read_sql_query(
+    'SELECT * FROM Teams;', con)
+
+team
+######################################################################################################################################################
+
+# %%,
+#########################
+# QUESTION 3
+# Tampa Bay Rays
+#########################
+query3 = pd.read_sql_query(
+    '''SELECT teamID, name, HR, yearID
+    FROM Teams
+    WHERE (yearID > '2000' AND name == 'Baltimore Orioles') OR (yearID > '2000' AND name == 'Boston Red Sox');''', 
+    con)
+
+query3
+
+#%%
+
+source = query3
+
+alt.Chart(source).mark_line().encode(
+    x='yearID:T',
+    y='HR:Q',
+    color='name:N',
+)
+
 # %%
