7b16b6bcca42256e20eb926b455d9ab2e6f7a6ca
SDorny <sarahdorny@gmail.com>
2023-10-24 13:44:06 -0600

    Project 3 almost done

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -7,115 +7,56 @@ alt.data_transformers.enable("vegafusion")
 con = sqlite3.connect('lahmansbaseballdb.sqlite')
 
 # %%
-#########################
-# QUESTION 1
-# schoolID = idbyuid
-# name_full = Brigham Young University-Idaho
-#########################
+"""Question 1"""
 query1 = pd.read_sql_query(
-    '''SELECT CollegePlaying.playerID, CollegePlaying.schoolID, Salaries.salary, CollegePlaying.yearID, Salaries.teamID
-    FROM CollegePlaying
-    JOIN Salaries
-    ON Salaries.playerID = CollegePlaying.playerID
-    WHERE CollegePlaying.schoolID = 'idbyuid'
-    ORDER BY Salaries.salary DESC;''', 
+    '''SELECT DISTINCT cp.playerID, cp.schoolID, s.salary, cp.yearID, s.teamID
+        FROM CollegePlaying cp
+        JOIN Salaries s
+        ON s.playerID = cp.playerID
+        WHERE cp.playerID = s.playerID AND schoolID = 'idbyuid'
+        ORDER BY s.salary DESC;''', 
     con)
 
 query1
-#%%
-###########################
-# Salaries Table
-###########################
-salaries = pd.read_sql_query(
-    'SELECT * FROM Salaries;', con)
-
-salaries
-#%%
-###########################
-# CollegePlaying Table
-###########################
-college = pd.read_sql_query(
-    'SELECT * FROM CollegePlaying;', con)
-
-college
-
 ######################################################################################################################################################
 #%%
-#########################
-# QUESTION 2A
-# Probably need to clean data
-#########################
+"""Question 2a"""
 query2 = pd.read_sql_query(
-    '''SELECT Salaries.playerID, CAST((Batting.H/Batting.AB) AS FLOAT) AS battingAvg, Salaries.yearID
-    FROM Batting
-    JOIN Salaries
-    ON Salaries.playerID = Batting.playerID
-    WHERE Batting.AB > 0 AND Salaries.yearID = 2001
-    ORDER BY battingAvg DESC
-    LIMIT 5;''', 
+    '''SELECT playerID, 1.0*H / AB AS battingAvg, H AS Hits, AB AS AtBats, yearID
+        FROM Batting
+        WHERE AB >= 1
+        ORDER BY battingAvg DESC
+        LIMIT 5;''', 
     con)
 
 query2
 
-
-#%%
-###########################
-# CollegePlaying Table
-###########################
-batting = pd.read_sql_query(
-    '''SELECT AB
-    FROM Batting
-    WHERE H > 0;''', con)
-
-batting
 # %%
-#########################
-# QUESTION 2B
-# Probably need to clean data
-#########################
+"""Question 2b"""
 query2 = pd.read_sql_query(
-    '''SELECT Salaries.playerID, CAST((Batting.H/Batting.AB) AS FLOAT) AS battingAvg, Salaries.yearID
-    FROM Batting
-    JOIN Salaries
-    ON Salaries.playerID = Batting.playerID
-    WHERE Batting.AB > 10 AND Salaries.yearID = 2001
-    ORDER BY battingAvg DESC
-    LIMIT 5;''', 
+    '''SELECT playerID, 1.0*H / AB AS battingAvg, H AS Hits, AB AS AtBats, yearID
+        FROM Batting
+        WHERE AB >= 100
+        ORDER BY battingAvg DESC
+        LIMIT 5;''', 
     con)
 
 query2
 # %%
-#########################
-# QUESTION 2C
-# Probably need to clean data
-#########################
+"""Question 2c"""
 query2 = pd.read_sql_query(
-    '''SELECT Salaries.playerID, CAST((Batting.H/Batting.AB) AS FLOAT) AS battingAvg, Salaries.yearID
-    FROM Batting
-    JOIN Salaries
-    ON Salaries.playerID = Batting.playerID
-    WHERE Batting.AB > 100
-    ORDER BY battingAvg DESC
-    LIMIT 5;''', 
+    '''SELECT playerID, 1.0*SUM(H) / SUM(AB) AS battingAvg, H AS Hits, AB AS AtBats
+        FROM Batting
+        GROUP BY playerID
+        HAVING SUM(AB) >= 100
+        ORDER BY battingAvg DESC
+        LIMIT 5;''', 
     con)
 
 query2
-
-#%%
-###########################
-# Teams Table
-###########################
-team = pd.read_sql_query(
-    'SELECT * FROM Teams;', con)
-
-team
 ######################################################################################################################################################
-
 # %%,
-#########################
-# QUESTION 3
-# Tampa Bay Rays
-#########################
+"""Question 3"""
 query3 = pd.read_sql_query(
     '''SELECT teamID, name, HR, yearID
     FROM Teams
@@ -123,15 +64,38 @@ query3 = pd.read_sql_query(
     con)
 
 query3
-
 #%%
-
 source = query3
-
 alt.Chart(source).mark_line().encode(
     x='yearID:T',
     y='HR:Q',
     color='name:N',
 )
 
-# %%
+#################################################################################################
+#%%
+"""Teams Table"""
+team = pd.read_sql_query(
+    'SELECT * FROM Teams;', con)
+
+team
+#%%
+"""Batting Table"""
+batting = pd.read_sql_query(
+    '''SELECT AB
+    FROM Batting
+    WHERE H > 0;''', con)
+
+batting
+#%%
+"""Salaries Table"""
+salaries = pd.read_sql_query(
+    'SELECT * FROM Salaries;', con)
+
+salaries
+#%%
+"""College Playing Table"""
+college = pd.read_sql_query(
+    'SELECT * FROM CollegePlaying;', con)
+
+college
