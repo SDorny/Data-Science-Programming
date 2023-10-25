257dc4cb253876064f83faa7e2876de48b39bf5e
SDorny <sarahdorny@gmail.com>
2023-10-25 15:38:15 -0600

    project 3 finished

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -24,7 +24,7 @@ query1
 query2 = pd.read_sql_query(
     '''SELECT playerID, 1.0*H / AB AS battingAvg, H AS Hits, AB AS AtBats, yearID
         FROM Batting
-        WHERE AB >= 1
+        WHERE AB >= 1 AND yearID = 2010
         ORDER BY battingAvg DESC
         LIMIT 5;''', 
     con)
@@ -36,7 +36,7 @@ query2
 query2 = pd.read_sql_query(
     '''SELECT playerID, 1.0*H / AB AS battingAvg, H AS Hits, AB AS AtBats, yearID
         FROM Batting
-        WHERE AB >= 100
+        WHERE AB >= 10 AND yearID = 2010
         ORDER BY battingAvg DESC
         LIMIT 5;''', 
     con)
@@ -45,10 +45,10 @@ query2
 # %%
 """Question 2c"""
 query2 = pd.read_sql_query(
-    '''SELECT playerID, 1.0*SUM(H) / SUM(AB) AS battingAvg, H AS Hits, AB AS AtBats
+    '''SELECT playerID, 1.0*SUM(H) / SUM(AB) AS battingAvg, SUM(H) AS Hits, SUM(AB) AS AtBats
         FROM Batting
         GROUP BY playerID
-        HAVING SUM(AB) >= 100
+        HAVING SUM(AB) > 100
         ORDER BY battingAvg DESC
         LIMIT 5;''', 
     con)
@@ -60,23 +60,37 @@ query2
 query3 = pd.read_sql_query(
     '''SELECT teamID, name, HR, yearID
     FROM Teams
-    WHERE (yearID > '2000' AND name == 'Baltimore Orioles') OR (yearID > '2000' AND name == 'Boston Red Sox');''', 
+    WHERE (yearID > '2006' AND name == 'New York Yankees') OR (yearID > '2006' AND name == 'Boston Red Sox');''', 
     con)
 
 query3
 #%%
 source = query3
-alt.Chart(source).mark_line().encode(
-    x='yearID:T',
-    y='HR:Q',
-    color='name:N',
+
+domain = ['New York Yankees', 'Boston Red Sox']
+range_ = ['#003087', '#BD3039']
+
+alt.Chart(
+    source,
+    title=alt.Title(
+        "The Battle For Homeruns in the Past Decade",
+        subtitle=["The two rivals follow a similar pattern,", "yet the Yankees remain on top."]
+    )).mark_line().encode(
+    alt.X('yearID:T').scale(zero=False).title('Year'),
+    alt.Y('HR:Q').scale(zero=False).title('# of Home Runs'),
+    color=alt.Color('name:N').title("Team Name").scale(domain=domain, range=range_),
+).configure_axis(
+    grid=False
+).configure_view(
+    stroke=None
 )
 
 #################################################################################################
 #%%
 """Teams Table"""
 team = pd.read_sql_query(
-    'SELECT * FROM Teams;', con)
+    '''SELECT * FROM Teams
+    WHERE name = "New York Yankees";''', con)
 
 team
 #%%
