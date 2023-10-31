d5a6dfe2ba2e3e4255dc7fa459190db64d265ca3
SDorny <sarahdorny@gmail.com>
2023-10-31 13:40:00 -0600

    project 4. New requirements.txt

---
+++ projects/project_4/project_4.py
@@ -0,0 +1,16 @@
+#%%
+import pandas as pd
+import altair as alt
+import numpy as np
+import seaborn as sns
+
+# %%
+denver = pd.read_csv('https://raw.githubusercontent.com/byuidatascience/data4dwellings/master/data-raw/dwellings_denver/dwellings_denver.csv')
+ml_dat = pd.read_csv('https://raw.githubusercontent.com/byuidatascience/data4dwellings/master/data-raw/dwellings_ml/dwellings_ml.csv')
+
+# %%
+print(denver.shape)
+print(ml_dat.shape)
+
+# %%
+print(ml_dat.columns)
